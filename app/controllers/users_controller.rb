cfbbade80faf501dc9b5ed2fc4e940565ca13f46
Nathan Appere <nathan.appere@gmail.com>
2012-02-06 22:23:17 -0800

    Few bug fixes. Adaptation of twitter bootstrap.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,11 +47,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
+        format.html { redirect_to users_url, notice: 'User was successfully created.' }
+        format.json { render json: users_url, status: :created, location: @user }
       else
         format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @users.errors, status: :unprocessable_entity }
       end
     end
   end
