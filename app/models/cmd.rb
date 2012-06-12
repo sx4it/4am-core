a1fc733c2f883c1084493925a55c592e5a06fc07
Bastien Chatelard <chatel.bast@gmail.com>
2012-06-12 16:21:57 +0200

    Changed cmd name to generic name

--- app/models/cmd.rb
+++ app/models/cmd.rb
@@ -1,5 +1,5 @@
 class Cmd
-  attr_accessor :id, :status, :time, :command, :log, :status_code, :hosts, :current_user, :hosts_id, :script
+  attr_accessor :id, :status, :time, :command, :log, :status_code, :hosts, :current_user, :hosts_id, :script, :type
 
   def initialize(hash = {})
     hash.each do |k, v|
@@ -8,32 +8,33 @@ class Cmd
     @time ||= Time.now
     @status ||= "started"
     @log ||= "-"
+    @type ||= "cmd-host"
   end
 
 
   def self.find(machine_id, id)
-      res = $redis.get "cmd-host:#{machine_id}:#{id}"
+      res = $redis.get "#{@type}:#{machine_id}:#{id}"
       return nil unless res
       Cmd.from_json(res)
   end
 
   def self.all(id)
       cmds = []
-      $redis.keys("cmd-host:#{id}:*").each do |c|
+      $redis.keys("#{@type}:#{id}:*").each do |c|
         cmds << self.find(id, c.split(':').last)
       end
       cmds
   end
 
   def destroy
-        $redis.del "cmd-host:#{@hosts[0].id}:#{@id}"
+        $redis.del "#{@type}:#{@hosts[0].id}:#{@id}"
   end
 
   def self.exec(machine_id, command, current_user)
     max = $redis.incr "#{machine_id}:max"
     cmd = Cmd.new(:command => Command.find(command), :hosts => [Machine.find(machine_id.to_i)], :id => max, :current_user => current_user)
-    $redis.set "cmd-host:#{machine_id}:#{max}", cmd.get_json
-    $redis.publish '4am-command', "cmd-host:#{machine_id}:#{max}"
+    $redis.set "#{@type}:#{machine_id}:#{max}", cmd.get_json
+    $redis.publish '4am-command', "#{@type}:#{machine_id}:#{max}"
     cmd
   end
 
@@ -63,11 +64,11 @@ class Cmd
 
   def stop
       @status = "stopped"
-      $redis.set "cmd-host:#{@hosts[0].id}:#{@id}", self.get_json
+      $redis.set "#{@type}:#{@hosts[0].id}:#{@id}", self.get_json
   end
 
   def self.clear(machine_id)
-    $redis.keys("cmd-host:#{machine_id}:*").each do |c|
+    $redis.keys("#{@type}:#{machine_id}:*").each do |c|
         Command.class
         Machine.class
         cmd = Cmd.from_json($redis.get c)
