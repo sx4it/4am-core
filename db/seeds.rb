07fdcc4ceaeb2cec6f01ad9cd15b7a0ba9c96263
Bastien Chatelard <chatel.bast@gmail.com>
2012-06-13 23:17:03 +0200

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
+#ading default user
 u = User.new
-u.login = admin
-u.password = admin
+u.login = 'admin'
+u.password = 'admin'
+u.roles << Role.create(:name => 'admin')
 u.save
+
+#ading default roles
+Role.create :name => 'edit'
+Role.create :name => 'exec'
+Role.create :name => 'view'
