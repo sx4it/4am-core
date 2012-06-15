25adcae12f980cfcc22a57e638bbd11b9ffc52f4
Bastien Chatelard <chatel.bast@gmail.com>
2012-06-15 15:54:18 +0200

    Added all host in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@
 class DashboardController < ApplicationController
   def index
     @hosts = Host.with_permissions_to(:show)
+    @hosts << HostGroup.with_permissions_to(:show)
+    @hosts = @hosts.flatten
   end
 end
