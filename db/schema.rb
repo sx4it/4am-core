9c40f5df762e09271de1229083ed2a6892b58967
Yann LEGER <yann@sx4it.com>
2012-06-26 21:39:17 -0700

    Fix regression in schema file

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(:version => 20120620040000) do
 
   create_table "keys", :force => true do |t|
     t.string   "name"
-    t.binary   "value",      :limit => 255
+    t.binary   "value",
     t.integer  "user_id"
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
