8c1cc4f591bf905c83015868e5900ff4302975d7
Yann LEGER <yann@sx4it.com>
2012-09-25 21:07:19 +0200

    Restored the ssl initializer

--- config/initializers/4am/ssl.rb
+++ config/initializers/4am/ssl.rb
@@ -1,10 +1,7 @@
 # Should be nicer, module/class/whatever
 
-#path = Rails.root.join(Rails.application.config.am['ssl']['ca_crt'])
-#$am_cacrt = OpenSSL::X509::Certificate.new File.read(path) # DER- or PEM-encoded
-$am_cacrt = nil
+path = Rails.root.join(Rails.application.config.am['ssl']['ca_crt'])
+$am_cacrt = OpenSSL::X509::Certificate.new File.read(path) # DER- or PEM-encoded
 
-#path = Rails.root.join(Rails.application.config.am['ssl']['ca_key'])
-#$am_cakey = OpenSSL::PKey::RSA.new File.read(path)
-$am_cakey = nil
-
+path = Rails.root.join(Rails.application.config.am['ssl']['ca_key'])
+$am_cakey = OpenSSL::PKey::RSA.new File.read(path)
