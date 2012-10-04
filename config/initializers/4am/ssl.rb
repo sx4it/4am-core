32ca2f143f95bc58e61a431a64b36bfcf05ed733
Bastien Chatelard <chatel.bast@gmail.com>
2012-10-04 16:02:45 +0200

    Added error handling for non cert usage

--- config/initializers/4am/ssl.rb
+++ config/initializers/4am/ssl.rb
@@ -1,7 +1,25 @@
 # Should be nicer, module/class/whatever
 
-path = Rails.root.join(Rails.application.config.am['ssl']['ca_crt'])
-$am_cacrt = OpenSSL::X509::Certificate.new File.read(path) # DER- or PEM-encoded
+ssl = Rails.application.config.am['ssl']
 
-path = Rails.root.join(Rails.application.config.am['ssl']['ca_key'])
-$am_cakey = OpenSSL::PKey::RSA.new File.read(path)
+if ssl
+  ca_crt = ssl['ca_crt']
+  if ca_crt
+    path = Rails.root.join(ca_crt)
+    $am_cacrt = OpenSSL::X509::Certificate.new File.read(path) # DER- or PEM-encoded
+  else
+    puts "cannot load certificate, please add ca_crt path to 4am.yml file"
+    $am_cacrt = nil
+  end
+
+  ca_key = ssl['ca_key']
+  if ca_key
+    path = Rails.root.join(ca_key)
+    $am_cakey = OpenSSL::PKey::RSA.new File.read(path)
+  else
+    puts "cannot load certificate, please add ca_key path to 4am.yml file"
+    $am_cakey = nil
+  end
+else
+  puts "Cannot read ssl informations in 4am.yml file."
+end
