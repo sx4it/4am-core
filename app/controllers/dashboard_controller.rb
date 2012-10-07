64816bb64f5ef9cc9b5699654cbce36ec4cf7e70
Bastien Chatelard <chatel.bast@gmail.com>
2012-10-07 15:46:49 +0200

    Minor ui fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@
 class DashboardController < ApplicationController
   def index
     @hosts = Host.with_permissions_to(:show)
-    @hosts << HostGroup.with_permissions_to(:show)
-    @hosts = @hosts.flatten
+    @host_group = HostGroup.with_permissions_to(:show)
   end
 end
