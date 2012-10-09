a4a4b5acbee71bdf2fadb17c2bbc1b806681ee3d
Bastien Chatelard <chatel.bast@gmail.com>
2012-10-09 23:33:58 +0200

    Added better form for host_group

--- app/models/host_group.rb
+++ app/models/host_group.rb
@@ -1,9 +1,27 @@
 class HostGroup < ActiveRecord::Base
-  attr_accessible :name
-  has_and_belongs_to_many :host, :after_add => :after_add_callback, :after_remove => :after_remove_callback
+  attr_accessible :name, :host_attributes
+  has_and_belongs_to_many :host, :after_add => :after_add_callback, :after_remove => :after_remove_callback, :uniq => true
   validates :name, :presence => true
   validates_uniqueness_of :name
   has_many :host_acl, :as => :hosts, :dependent => :destroy
+  accepts_nested_attributes_for :host, :allow_destroy => true
+
+  def host_attributes=(attrs)
+    hosts = []
+    attrs.each do |key, attr|
+      break if attrs[key][:id] == ""
+      if attrs[key][:_destroy] == "1"
+        self.host.delete Host.find(attrs[key][:id])
+      else
+        hosts << Host.find(attrs[key][:id])
+      end
+      # trick to be sure the final action is adding
+      # and not deleting (in case we delete and add in the same submit)
+      hosts.each do |host|
+        self.host << host
+      end
+    end
+  end
 
   def after_add_callback(record)
     self.host_acl.all.each do |acl|
