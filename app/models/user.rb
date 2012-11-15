c4cce779e0d6cc923ff269e2e70d484d312ffae1
Bastien Chatelard <chatel.bast@gmail.com>
2012-11-15 13:03:44 +0100

    Patch message

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :roles_attributes, :password, :password_confirmation, :login, :email
 
-  validates :login, :exclusion => {:in => %w{root}, :message => "Reserved login."}
+  validates :login, :exclusion => {:in => %w{root}, :message => "Reserved"}
 
   acts_as_authentic do |c|
     # only for tests
